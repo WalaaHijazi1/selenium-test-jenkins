c2ca855b315aa2d4779ec2b3cc6b86ab3ef1c0b3
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-03-10 22:14:06 +0200

    Selenium-Test on Jenkins

---
+++ python_first.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+
+
+
+# Create a new instance of the Chrome driver
+
+driver = webdriver.Chrome('./chromedriver-win64/chromedriver.exe')
+
+
+
+# Open the Python website
+
+driver.get("https://www.youtube.com")
+
+
+
+# Print the page title
+
+print(driver.title)
+
+
+
+# Find the search bar using its name attribute
+
+search_bar = driver.find_element_by_name("q")
+
+search_bar.clear()
+
+search_bar.send_keys("getting started with python")
+
+search_bar.send_keys(Keys.RETURN)
+
+
+
+# Print the current URL
+
+print(driver.current_url)
+
+
+
+# Close the browser window
+
+driver.close()
